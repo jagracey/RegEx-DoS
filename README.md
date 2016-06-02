a204c3d105f215309fee1da2eb035443f254059b
John Gracey <jagracey@gmail.com>
2016-06-01 17:57:49 -0700

    :smile: Initial Upload & Implementation

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Regex-DoS
-:cop: :punch: Regex Denial of Service (ReDos) Scanner
+:cop: :punch: RegEx Denial of Service (ReDos) Scanner
+
+As usual, install with NPM.
+```
+npm install redos
+```
+
+You can run redos on the CLI:
+```bash
+# Use "Find" to run Regex-DoS for any set of JS files you want.
+find . -name "*.js" -not -path "./node_modules/*" -exec node app.js {} \;
+```
+
+
+Or to run as a node module:
+```javascript
+var redos = require('redos');
+
+// Using a Callback
+redos(" 'aaaa'.split(/a+b?c*/g); ", function(rNodes){
+  console.log( rNodes.results() );
+};
+
+
+// Or without a Callback
+redos(" 'aaaa'.split(/a+b?c*/g); ").results();
+
+
+// Or with some better content to parse
+const fs      = require('fs');
+const content = fs.readFileSync(file);
+redos( content ).results();
+
+```
---
+++ redos.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+'use strict';
+
+
+const fs      = require('fs');
+const chalk   = require('chalk');
+const esprima = require('esprima');
+const safeReg    = require('safe-regex');
+
+
+function traverse(node, work) {
+  work(node);
+  for (let key in node) {
+    if (node.hasOwnProperty(key)) {
+        let child = node[key];
+        if (typeof child === 'object' && child !== null) {
+          if (Array.isArray(child))
+            child.forEach(node => traverse(node, work); );
+          else
+            traverse(child, work);
+        }
+    }
+  }
+}
+
+
+const parse = function(content, cb){
+  const regexNodes = new RegexNodes();
+  const ast = esprima.parse( content, {
+    sourceType: 'module',
+    loc: true
+  });
+
+  traverse( ast, function(node) {
+    if (node.regex && node.regex.pattern)
+      regexNodes.add( new Node(node) );
+  });
+
+  if ( typeof cb === 'function')
+    cb(regexNodes);
+  else
+    return regexNodes;
+};
+
+
+const utility = {
+  rPad: function(str, n, char, max){ return str + Array( Math.max(n - str.length, 0)).slice(0,max).join(' '); },
+  lPad: function(str, n, char, max){ return       Array( Math.max(n - str.length, 0)).slice(0,max).join(' ') + str;}
+};
+
+
+const RegexNodes = function(){
+  const self = this;
+  const nodes = [];
+
+  this.get = i => nodes[i];
+  this.add = node => nodes.push(node);
+  this.results = () => nodes.map( n=>n.getData() );
+
+  // Not important, simply gets the max string lenght of the pattern for formatting output. not really used.
+  this.maxPatternLength = function(){
+    return nodes.reduce((max,n)=> Math.max(max,n.pattern().length), 0);
+  }
+  this.printAll = function(){
+    nodes.map(n=>console.log(n.format()) );
+  };
+};
+
+
+const Node = function(node){
+  // Since Nodes are static, there isn't too much of a point adding functions,
+  // but it does make formatting strings cleaner.
+  const safe  = safeReg(node.regex.pattern);
+  const color = safe ? chalk.green: chalk.red;
+  const pattern = node.regex.pattern; // Redundant, but cleaner...
+
+  const colorPattern = () => color(pattern);
+  this.pattern       = () => pattern;
+  this.getData       = () =>({ safe, pattern, loc: node.loc });
+
+  this.formatLine = function(){
+    const start = node.loc.start;
+    const end   = node.loc.end;
+    return 'Line[' + start.line +':'+ start.column +'->'+ end.line +':'+ end.column+']';
+  };
+
+  this.toString = function(){
+    return  chalk.gray( this.formatLine() ) +'  '+ colorPattern();
+  };
+  this.format = function(){
+    return  chalk.gray( utility.lPad( this.formatLine(),34) ) +'  '+ colorPattern();
+  }
+};
+
+
+  // Do if running directly from CLI.
+if (require.main === module) {
+  const file    = process.argv[2];
+  const content = fs.readFileSync(file);
+  const rNodes = new RegexNodes();
+
+  parse(content, function(rNodes){
+    console.log( chalk.blue('Processing:'), chalk.white(file));
+    rNodes.printAll();
+  });
+}
+
+
+module.exports = parse;
