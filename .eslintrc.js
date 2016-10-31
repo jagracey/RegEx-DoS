7c5fbd15ef62bbbf603e4083ce945a0bf9cbb723
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-10-31 22:26:20 +0100

    Format and lint code

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "no-console": 0,
+        "no-continue": 0,
+        "no-restricted-syntax": 0,
+        "no-prototype-builtins": 0
+    }
+};
